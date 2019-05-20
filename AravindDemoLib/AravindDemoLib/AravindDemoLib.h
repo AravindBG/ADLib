854b754a08a8064f36691d59892ed6306fd5d129
Aravind <bg.aravind1@gmail.com>
2019-05-20 13:02:26 +0530

    - Project setup commit.

---
+++ AravindDemoLib/AravindDemoLib/AravindDemoLib.h
@@ -0,0 +1,19 @@
+//
+//  AravindDemoLib.h
+//  AravindDemoLib
+//
+//  Created by Aravind on 20/05/19.
+//  Copyright © 2019 Aravind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AravindDemoLib.
+FOUNDATION_EXPORT double AravindDemoLibVersionNumber;
+
+//! Project version string for AravindDemoLib.
+FOUNDATION_EXPORT const unsigned char AravindDemoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AravindDemoLib/PublicHeader.h>
+
+
